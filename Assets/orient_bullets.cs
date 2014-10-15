0805d3092ea34d644cd913b51dde2636c571e164
tjchamp <theodorechamplin@0587366155.wireless.umich.net>
2014-10-15 14:46:10 -0400

    Updated bullets on screen

--- Assets/orient_bullets.cs
+++ Assets/orient_bullets.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class orient_bullets : MonoBehaviour {
+
+	public GameObject[] bullets;
 
 	public float t = 0;
 
 	// Use this for initialization
 	void Start () {
-		transform.renderer.enabled = false;
-		transform.LookAt (Camera.main.transform);
+		//transform.LookAt (Camera.main.transform);
 		foreach(Transform child in transform){
 			child.renderer.enabled = false;
 		}
@@ -17,8 +19,13 @@ public class orient_bullets : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Time.time > 11f){
-			transform.LookAt (Camera.main.transform);
+			//transform.LookAt (Camera.main.transform);
+
+			int bulletShowIndex = 0;
+			int bulletCount = GameObject.Find ("Player").GetComponent<PlayerMove>().bulletCount;
+
 			foreach(Transform child in transform){
+				if(bulletShowIndex++ == bulletCount) break;
 				child.renderer.enabled = true;
 			}
 		}
