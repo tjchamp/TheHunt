a637f1a3ffd8fbe39d33335e468fd5dc02149ec2
tjchamp <theodorechamplin@0587366368.wireless.umich.net>
2014-10-16 15:43:25 -0400

    updated bullet view

--- Assets/Scripts/CameraMotion.cs
+++ Assets/Scripts/CameraMotion.cs
@@ -15,6 +15,23 @@ public class CameraMotion : MonoBehaviour {
 			position.y += .025f * Mathf.Sin(Time.time * 6f * Mathf.PI);
 			Camera.main.transform.position = position;
 		}
+
+		if(Time.timeSinceLevelLoad > 11f) {
+
+			int i = 0;
+			int bulletCount = GameObject.Find ("Player").GetComponent<PlayerMove>().bulletCount;
+			
+			foreach(Transform child in transform){
+				if(i++ == bulletCount) break;
+				Vector3 v3Pos; 
+				v3Pos.x = 0.85f + 0.0675f * Mathf.Cos(i * Mathf.PI/3);
+				v3Pos.y = 0.15f + 0.0875f * Mathf.Sin(i * Mathf.PI/3); 
+				v3Pos.z = 2.5f;
+				child.transform.position = Camera.main.camera.ViewportToWorldPoint(v3Pos);
+				child.renderer.enabled = true;
+			}
+
+		}
 	
 	}
 }
